e461bbfdbaa8e36153c1b086c7e8f0c554faf336
Carlos Gordillo <cgordillomolina@gmail.com>
2026-07-31 22:40:21 -0500

    Django Admin (Autorización)

--- dashboard/views.py
+++ dashboard/views.py
@@ -2,8 +2,10 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.decorators import login_required, permission_required
 
 @login_required
+@permission_required('dashboard.index_viewer', raise_exception=True)
 def index(request):
     data = {
         'title': "Landing Page' Dashboard",
